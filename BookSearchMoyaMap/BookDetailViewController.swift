9b8019d7e92b5e50d865e6f30057e796ee1aae1d
uejo <rentarou1994@gmail.com>
2023-05-11 09:21:32 +0900

    :beer: 詳細画面を作る

--- BookSearchMoyaMap/BookDetailViewController.swift
+++ BookSearchMoyaMap/BookDetailViewController.swift
@@ -12,9 +12,30 @@ class BookDetailViewController: UIViewController {
     
     var bookData: VolumeInfo?
     
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var webButton: UIButton!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var publishDateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let _bookData = bookData {
+            self.navigationItem.title = _bookData.title
+            self.titleLabel.text = _bookData.title
+            self.authorLabel.text = _bookData.authors?.first
+            self.publishDateLabel.text = _bookData.publishedDate
+            self.descriptionTextView.text = _bookData.description
+            self.descriptionTextView.sizeToFit()
+        }
     }
     
 }
